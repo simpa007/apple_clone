b9070e821d594e8fdaf938e950251d68567d8d14
simpa007 <simpaimmey@gmail.com>
2024-03-25 12:30:11 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlight from "./components/Highlight";
+function App() {
+	return (
+		<main>
+			<Navbar />
+			<Hero />
+			<Highlight />
+		</main>
+	);
+}
+
+export default App;
