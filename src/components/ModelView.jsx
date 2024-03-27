e54e7c1b325a8db2b67ab50fd713efb5578e84eb
simpa007 <simpaimmey@gmail.com>
2024-03-27 06:12:23 -0700

    fourth commit

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,56 @@
-import React from "react";
-
-const ModelView = () => {
-	return <div>ModelView</div>;
+import React, { Suspense } from "react";
+import {
+	Html,
+	OrbitControls,
+	PerspectiveCamera,
+	View,
+} from "@react-three/drei";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+const ModelView = ({
+	index,
+	groupRef,
+	gsapType,
+	controlRef,
+	setRotationState,
+	item,
+	size,
+}) => {
+	return (
+		<View
+			index={index}
+			id={gsapType}
+			className={`border-2 border-red-500 w-full h-full ${
+				index === 2
+			} ? "right-[-100%]" : ""`}
+		>
+			// Ambient Light
+			<ambientLight intensity={0.3} />
+			<PerspectiveCamera makeDefault position={[0, 0, 4]} />
+			<Lights />
+			<OrbitControls
+				makeDefault
+				ref={controlRef}
+				enableZoom={false}
+				enablePan={false}
+			/>
+			<group
+				ref={groupRef}
+				name={`${index == 1} ? "small" : "large"`}
+				position={[0, 0, 0]}
+			>
+				<Suspense
+					fallback={
+						<Html>
+							<div>Loading</div>
+						</Html>
+					}
+				>
+					<IPhone />
+				</Suspense>
+			</group>
+		</View>
+	);
 };
 
 export default ModelView;
