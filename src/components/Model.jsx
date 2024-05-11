39a2efac13c545d1fab833d47bebbfe6d9784e06
simpa007 <simpaimmey@gmail.com>
2024-05-11 13:02:20 -0700

    finished project

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,5 +1,5 @@
 import { useGSAP } from "@gsap/react";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
@@ -7,6 +7,7 @@ import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import * as THREE from "three";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
 	const [size, setSize] = useState("small");
@@ -25,6 +26,22 @@ const Model = () => {
 	//rotation
 	const [smallRotation, setSmallRotation] = useState(0);
 	const [largeRotation, setLargeRotation] = useState(0);
+
+	const tl = gsap.timeline();
+	useEffect(() => {
+		if (size === "large") {
+			animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+				transform: "translateX(-100%)",
+				duration: 2,
+			});
+		}
+		if (size === "small") {
+			animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+				transform: "translateX(0)",
+				duration: 2,
+			});
+		}
+	}, [size]);
 
 	useGSAP(() => {
 		gsap.to("#heading", { y: 0, opacity: 1 });
